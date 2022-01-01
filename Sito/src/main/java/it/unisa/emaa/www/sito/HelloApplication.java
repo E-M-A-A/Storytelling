564e0284258c1c091f4ddb27151315c7cd4f9b11
Muriel <m.rossi67@studenti.unisa.it>
2022-01-01 17:32:40 +0100

    Prima creazione

---
+++ Sito/src/main/java/it/unisa/emaa/www/sito/HelloApplication.java
@@ -0,0 +1,9 @@
+package it.unisa.emaa.www.sito;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}

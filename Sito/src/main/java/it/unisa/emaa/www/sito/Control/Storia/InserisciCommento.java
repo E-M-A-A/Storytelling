f23b443de40c064bc54faf8d0b151cfcc0a9fe71
AleMarigliano16 <a.marigliano16@studenti.unisa.it>
2022-01-03 17:45:20 +0100

    Merge

--- Sito/src/main/java/it/unisa/emaa/www/sito/Control/Storia/InserisciCommento.java
+++ Sito/src/main/java/it/unisa/emaa/www/sito/Control/Storia/InserisciCommento.java
@@ -11,10 +11,10 @@ public class InserisciCommento extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         HttpSession session = req.getSession();
-        Utente user = session.getAttribute("utente");
+        Utente utente = session.getAttribute("utente");
         int idStoria = Integer.parseInt(req.getParameter("storia"));
         String commento = req.getParameter("commento");
         CommentoDao commentoDao = new CommentoDao();
-        commentoDao.doCreate(commento,idStoria);
+        commentoDao.doCreate(utente,commento,idStoria);
     }
 }
--- Sito/src/main/java/it/unisa/emaa/www/sito/Control/Utente/Asincrono/ValidaUsername.java
+++ Sito/src/main/java/it/unisa/emaa/www/sito/Control/Utente/Asincrono/ValidaUsername.java
@@ -3,11 +3,13 @@ package it.unisa.emaa.www.sito.Control.Utente.Asincrono;
 import it.unisa.emaa.www.sito.Utils.Validazione;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet(name="UsernamePresente",urlPatterns = "/UsernamePresente")
 public class ValidaUsername extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {

873f7dfd092737effeec5099abaf565afbe92517
AleMarigliano16 <a.marigliano16@studenti.unisa.it>
2022-01-03 13:28:50 +0100

    Merge

--- Sito/src/main/java/it/unisa/emaa/www/sito/Control/Utente/RegistrazioneUtente.java
+++ Sito/src/main/java/it/unisa/emaa/www/sito/Control/Utente/RegistrazioneUtente.java
@@ -1,6 +1,6 @@
 package it.unisa.emaa.www.sito.Control.Utente;
 
-import it.unisa.emaa.www.sito.Control.Utils.Validazione;
+import it.unisa.emaa.www.sito.Utils.Validazione;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;

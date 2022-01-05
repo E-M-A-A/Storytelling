1e3a15048d9f1d7fa08af92af243d5aad2747e39
AleMarigliano16 <a.marigliano16@studenti.unisa.it>
2022-01-05 17:34:33 +0100

    Merge

--- Sito/src/main/java/it/unisa/emaa/www/sito/Control/Storia/PubblicaStoria.java
+++ Sito/src/main/java/it/unisa/emaa/www/sito/Control/Storia/PubblicaStoria.java
@@ -1,11 +1,17 @@
 package it.unisa.emaa.www.sito.Control.Storia;
 
+import it.unisa.emaa.www.sito.Model.dao.StoriaDao;
+import it.unisa.emaa.www.sito.Model.entity.Storia;
+import it.unisa.emaa.www.sito.Model.entity.Utente;
+import sun.util.resources.LocaleData;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.time.LocalDate;
 
 public class PubblicaStoria extends HttpServlet {
     @Override
@@ -18,6 +24,12 @@ public class PubblicaStoria extends HttpServlet {
 
     private boolean pubblicaStoria(String username,String contenuto){
         StoriaDao storiaDao = new StoriaDao();
-        storiaDao.doSave(username(),contenuto);
+        Storia storia = new Storia();
+        storia.setContenuto(contenuto);
+        storia.setUsername(username);
+        storia.setNCommenti(0);
+        storia.setNReazioni(0);
+        storia.setDataCreazione(LocalDate.now());
+        storiaDao.doSave(storia);
     }
 }

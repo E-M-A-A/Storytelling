7c21a5eb73190adde68eb749501283a996ec52c5
AleMarigliano16 <a.marigliano16@studenti.unisa.it>
2022-01-13 16:42:09 +0100

    Merge

--- Sito/src/main/java/it/unisa/emaa/www/sito/Control/Storia/VisualizzaBacheca.java
+++ Sito/src/main/java/it/unisa/emaa/www/sito/Control/Storia/VisualizzaBacheca.java
@@ -1,6 +1,7 @@
 package it.unisa.emaa.www.sito.Control.Storia;
 
 import com.google.gson.Gson;
+import it.unisa.emaa.www.sito.Control.Utente.Asincrono.CaricaStorie;
 import it.unisa.emaa.www.sito.Model.dao.ReazioneDao;
 import it.unisa.emaa.www.sito.Model.dao.StoriaDao;
 import it.unisa.emaa.www.sito.Model.entity.Storia;
@@ -14,7 +15,6 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Hashtable;
 import java.util.List;
 
 /**
@@ -22,7 +22,7 @@ import java.util.List;
  * Viene richiesto il numero di pagina per selezionare solo una parte delle storie da visualizzare.
  * @author Alessandro Marigliano
  */
-@WebServlet(name = "VisualizzaBacheca",urlPatterns = "/VisualizzaBacheca")
+@WebServlet(name = "VisualizzaHome",urlPatterns = "/VisualizzaHome")
 public class VisualizzaBacheca extends HttpServlet {
     private StoriaDao storiaDao;
     private ReazioneDao reazioneDao;
@@ -40,27 +40,18 @@ public class VisualizzaBacheca extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         int pagina = Integer.parseInt(req.getParameter("pagina"));
         String email = req.getParameter("email");
-        HashMap<Storia,Boolean> storieReazioni = new HashMap<>();
-        ArrayList<Storia> listaStorie = (ArrayList<Storia>) recuperaListaStorie(pagina);
-        boolean present;
-        for(Storia s: listaStorie){
-            present = Validazione.reactionIsPresent(email, s.getId(), reazioneDao);
-            storieReazioni.put(s,present);
-        }
+        HashMap<Storia,Boolean> storieReazioni = new CaricaStorie(storiaDao,reazioneDao).recuperaListaStorie(pagina,email);
         Gson gson = new Gson();
         String json = gson.toJson(storieReazioni);
         resp.setContentType("plain/text");
         resp.setCharacterEncoding("UTF-8");
         resp.getWriter().print(json);
     }
-    private List<Storia> recuperaListaStorie(int pagina){
-        return storiaDao.doRetrieveByPage(pagina*30,30);
-    }
     public VisualizzaBacheca(){
         storiaDao = new StoriaDao();
         reazioneDao = new ReazioneDao();
     }
-    public VisualizzaBacheca(StoriaDao storiaDao,ReazioneDao reazioneDao){
+    public VisualizzaBacheca(StoriaDao storiaDao, ReazioneDao reazioneDao){
         this.storiaDao = storiaDao;
         this.reazioneDao = reazioneDao;
     }
---
+++ Sito/src/main/java/it/unisa/emaa/www/sito/Control/Utente/Asincrono/CaricaStorie.java
@@ -0,0 +1,50 @@
+package it.unisa.emaa.www.sito.Control.Utente.Asincrono;
+
+import com.google.gson.Gson;
+import it.unisa.emaa.www.sito.Model.dao.ReazioneDao;
+import it.unisa.emaa.www.sito.Model.dao.StoriaDao;
+import it.unisa.emaa.www.sito.Model.entity.Storia;
+import it.unisa.emaa.www.sito.Utils.Validazione;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class CaricaStorie extends HttpServlet {
+    private StoriaDao storiaDao;
+    private ReazioneDao reazioneDao;
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String email = req.getParameter("email");
+        int pagina = Integer.parseInt("pagina");
+        HashMap<Storia, Boolean> storieReazioni = recuperaListaStorie(pagina,email);
+        Gson gson = new Gson();
+        String json = gson.toJson(storieReazioni);
+        resp.setContentType("plain/text");
+        resp.setCharacterEncoding("UTF-8");
+        resp.getWriter().print(json);
+    }
+
+    public HashMap<Storia,Boolean> recuperaListaStorie(int pagina, String email) {
+        HashMap<Storia, Boolean> storieReazioni = new HashMap<>();
+        ArrayList<Storia> listaStorie = (ArrayList<Storia>) storiaDao.doRetrieveByPage(30,pagina*30);
+        boolean present;
+        for (Storia s : listaStorie) {
+            present = Validazione.reactionIsPresent(email, s.getId(), reazioneDao);
+            storieReazioni.put(s, present);
+        }
+        return storieReazioni;
+    }
+    public CaricaStorie(){
+        storiaDao = new StoriaDao();
+        reazioneDao = new ReazioneDao();
+    }
+    public CaricaStorie(StoriaDao storiaDao,ReazioneDao reazioneDao){
+        this.storiaDao = storiaDao;
+        this.reazioneDao = reazioneDao;
+    }
+}

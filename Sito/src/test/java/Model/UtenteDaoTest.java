bceff649bd4266c8f0eb7dc4469019e44eaed849
EmmanuelEngineer <e.coppola37@studenti.unisa.it>
2022-01-13 15:19:36 +0100

    Cambio nome parametri

---
+++ Sito/src/test/java/Model/UtenteDaoTest.java
@@ -0,0 +1,35 @@
+package Model;
+
+import it.unisa.emaa.www.sito.Model.ConnPool;
+import it.unisa.emaa.www.sito.Model.dao.UtenteDao;
+import org.junit.Test;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**La classe si occupa di testare la classe UtenteDao nei metodi di communicazione con il database
+ * @author Emmanuele Virginio Coppola
+ */
+public class UtenteDaoTest{/*
+    UtenteDao dao = new UtenteDao();
+   Connection connection;
+   @BeforeAll
+    void setup() throws SQLException {
+       connection= ConnPool.getConnection();
+   }
+   @AfterEach
+   void rimozioneDatiTesting() throws SQLException {
+       Statement statement = connection.createStatement();
+       String sql;
+   }
+   @Test
+    void testRetrieveByUsername(){
+       Statement statement =connection.prepareStatement();
+   }
+   */
+
+}
